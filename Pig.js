e6ccaa38715dae287edb8012b81a4f59859cd685
Gurpreet singh Mehrok <gurpreetsinghmehrok@Gurpreets-MacBook-Air.local>
2020-12-21 14:45:04 -0800

    testing

--- Pig.js
+++ Pig.js
@@ -2,6 +2,24 @@ class Pig extends BaseClass {
   constructor(x, y){
     super(x,y,50,50);
     this.image = loadImage("sprites/enemy.png");
+    this.visibilty=255;
+  }
+
+
+
+  display(){
+    if(this.body.speed <3){
+    super.display()
+    console.log(this.body.speed);
+    }
+    else{
+      push();
+      World.remove(world, this.body);
+      this.visibility -=5;
+      tint(255, this.visibilty);
+      image(this.image, this.body.position.x, this.body.position.y, 50,50)
+      pop();
+    }
   }
 
 };
